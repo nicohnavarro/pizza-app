02354900d58e24652b6d72fa337bf8d9f8bdf596
nicohnavarro <nicohnavarro@gmail.com>
2021-06-30 00:34:18 -0300

    :fire: add Delivery details

--- src/app/components/country/country-info/country-info.component.ts
+++ src/app/components/country/country-info/country-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Country } from './../../../models/Country';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-country-info',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryInfoComponent implements OnInit {
 
+  @Input() country:Country | undefined
   constructor() { }
 
   ngOnInit(): void {
