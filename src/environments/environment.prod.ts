017d63cac5f20ab17769b53b1e55a63ad8876627
nicohnavarro <nicohnavarro@gmail.com>
2021-07-03 01:20:25 -0300

    :rocket: deploy heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDsmBWlUW8Xu2KLjf1lbyEOAKEvdoJwubc",
+    authDomain: "nicohnavarro-clones.firebaseapp.com",
+    projectId: "nicohnavarro-clones",
+    storageBucket: "nicohnavarro-clones.appspot.com",
+    messagingSenderId: "830974104291",
+    appId: "1:830974104291:web:8efe271505815f4f46ebd6",
+    measurementId: "G-EF3DG6RKJS"
+  }
 };
